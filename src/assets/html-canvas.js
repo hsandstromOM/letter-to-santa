ec028e83f5120ade4b4e2d04978eb3f7b9efb2ef
Hosea Sandstrom <hoseasandstrom@Hoseas-MacBook-Pro.local>
2018-12-10 22:42:51 -0500

    testing document.write for mobile users

--- src/assets/html-canvas.js
+++ src/assets/html-canvas.js
@@ -1,36 +1,4 @@
-// $(function() {
-//   $('#save').click(function() {
-//     html2canvas($('#story')[0], {
-//       scale: 2
-//     }).then(function(canvas) {
-//       var a = document.createElement('a');
-//       a.href = canvas.toDataURL("image/png");
-//       a.download = 'myfile.png';
-//       a.click();
-//     });
-//   });
-// });
-//
-// function saveAs(uri, filename) {
-//   var link = document.createElement('a');
-//   if (typeof link.download === 'string') {
-//     link.href = uri;
-//     link.download = filename;
-//
-//     //Firefox requires the link to be in the body
-//     document.body.appendChild(link);
-//
-//     //simulate click
-//     link.click();
-//
-//     //remove the link when done
-//     document.body.removeChild(link);
-//   } else {
-//     window.open(uri);
-//   }
-// }
-// });
- $(function() {
+$(function() {
   $("#btnSave").click(function() {
     html2canvas($("#widget"), {
       onrendered: function(canvas) {
@@ -39,23 +7,16 @@
     });
   });
 
-  // $("#btnSave2").click(function() {
-  //   html2canvas($("#widget"), {
-  //     onrendered: function(canvas) {
-  //       saveAs(canvas.toDataURL(), 'canvas.png');
-  //     }
-  //   });
-  // });
-
   $('#save').click(function() {
      html2canvas($('#story')[0], {
-       scale: 2
+       scale: 1.5
      }).then(function(canvas) {
-       var a = document.createElement('a');
+      var g = document.write('<img style="width:100%; height:auto;" src="' + canvas.toDataURL() + '" />');
+      var a = document.createElement('a');
        document.body.appendChild(a);
        a.href = canvas.toDataURL("image/png");
        a.download = 'letter-to-santa.png';
-       a.click();
+       // a.click();
      });
    });
 
